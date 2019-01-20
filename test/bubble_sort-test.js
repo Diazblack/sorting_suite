22c9e1d79880433621028bdebb0d506c950ef11f
Cesar Jolibois <diazcd85@gmail.com>
2019-01-20 12:47:49 -0700

    created test and script but test don't work

--- test/bubble_sort-test.js
+++ test/bubble_sort-test.js
@@ -1,10 +1,33 @@
 const assert = require('chai').assert
 const bubbleSort = require('../lib/bubble_sort')
+console.log(bubbleSort)
 
 describe('Sorting Algorithm ', function() {
   context('giving and array it will sort it ', function(){
+    it('return an array of 1 value', function(){
+      assert.deepEqual(bubbleSort([5]), [5]);
+    })
     it('from smallest to biggest', function(){
-      assert.equal(bubbleSort([5,3,4,2,1]), [1,2,3,4,5])
+      array = [3,1,2];
+      assert.deepEqual(bubbleSort(array), [1,2,3]);
+    })
+    it('from smallest to biggest take 2', function(){
+      array = [2,3,1];
+      assert.deepEqual(bubbleSort(array), [1,2,3]);
+    })
+
+    it('from smallest to biggest take 3', function(){
+      array = [1,3,2];
+      assert.deepEqual(bubbleSort(array), [1,2,3]);
+    })
+
+    it('from sorted array', function(){
+      array = [1,2,3];
+      assert.deepEqual(bubbleSort(array), [1,2,3]);
+    })
+    it('sort duplicate values ', function() {
+      array = [5,3,4,2,5,4,3,2,1,1];
+      assert.deepEqual(bubbleSort(array), [1,1,2,2,3,3,4,4,5,5]);
     })
   })
 })
